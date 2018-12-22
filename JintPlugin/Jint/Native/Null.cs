72337aa7bc4f5f46d45acc1ec1863ada856ad5ca
DreTaX <dretax@gmail.hu>
2018-12-22 17:42:16 +0100

    Removing unnecessary files.
    Signed-off-by: DreTaX <dretax@gmail.hu>

--- JintPlugin/Jint/Native/Null.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Jint.Native
-{
-    public static class Null
-    {
-        public static JsValue Instance = JsValue.Null;
-        public static string Text = "null";
-    }
-}
--- JintPlugin/PlayerLog/README.txt
+++
@@ -1,2 +0,0 @@
-PlayerLog is a simple plugin demonstrating some of the features
-that JintPlugin and Jint2.2 support differently than Magma and Jint 0.9.
